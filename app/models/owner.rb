dddd8414c9a4a552ef2a55d1964cbf7f20c3325f
BrihanArrietaR <brihan.arrieta@ucrso.info>
2022-05-03 22:26:33 -0600

    LAB2-B90736 My first LAB2-B90736

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,6 +2,6 @@ class Owner < ApplicationRecord
     validates :name, length: { maximum: 10 }, presence: true 
     validates :last_name, length: { maximum: 15 }, presence: true
     validates :email, presence: true, email: true
-    validates :identification_number, length: { maximum: 10, minimum: 5 }, presence: true, uniqueness: true
+    validates :identification_number, length: { maximum: 8, minimum: 5 }, presence: true, uniqueness: true
     has_many :pets
 end
