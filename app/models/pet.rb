6a36378fd4038e592802e018aeaa9edd5339ece3
iOrlandoA <orlando.aguilar@ucrso.info>
2022-05-05 00:33:00 -0600

    Lab Completo

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ApplicationRecord
-    
+    belongs_to :Owner
     validates :name, presence: true, length: {minimum: 1, maximum: 10}
     validates :breed, presence: true, length: {minimum: 1, maximum: 10} 
     validates :pedigree, inclusion: [true, false]
