6a5a0fa5028c45379512f1891434d2d44492fb0e
iOrlandoA <orlando.aguilar@ucrso.info>
2022-05-09 18:16:58 -0600

    Internationalization first step

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Lenguajes1
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
+    I18n.load_path +=Dir[Rails.root.join('config','locales','*.{yml}')]
+    I18n.default_locale = :es
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
